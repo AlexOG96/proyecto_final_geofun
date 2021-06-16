d4f06fe5b09ad6899e8bd9b8c2c67dd262118e5b
Alex <sylux@hotmail.es>
2021-06-16 17:06:20 +0200

    verificacion google2

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,12 @@ class Index extends React.Component {
       
       <div>
         <head>
-        <script async
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDlHJEzjw5cJBiw6-6R-0s2pd55sf_0W08&libraries=places&callback=initMap">
-        </script>
+        <meta name="google-site-verification" content="ahXrxK6Glgt4H-L8WEsxZxyHD6AX6blnMHpBbB8XUwk" />
+        {/*
+          <script async
+              src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDlHJEzjw5cJBiw6-6R-0s2pd55sf_0W08&libraries=places&callback=initMap">
+          </script>
+        */} 
         </head>
         <Header/>
         <Result
